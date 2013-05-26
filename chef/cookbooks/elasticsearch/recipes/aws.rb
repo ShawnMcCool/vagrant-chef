e59df75bacd764ae1f3d15bfe00bbfc743f5186d
Shawn McCool <shawn@heybigname.com>
2013-05-26 21:17:33 +0200

    removed recipe folders

--- chef/cookbooks/elasticsearch/recipes/aws.rb
+++
@@ -1,3 +0,0 @@
-node.default[:elasticsearch][:plugin][:mandatory] = Array(node[:elasticsearch][:plugin][:mandatory] | ['cloud-aws'])
-
-install_plugin "elasticsearch/elasticsearch-cloud-aws/#{node.elasticsearch['plugins']['elasticsearch-cloud-aws']['version']}"
--- chef/cookbooks/xfs/CHANGELOG.md
+++
@@ -1,3 +0,0 @@
-## v1.1.0:
-
-* [COOK-2076] - Add Amazon Linux support
