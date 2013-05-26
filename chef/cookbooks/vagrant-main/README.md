ece1fb33f7a76f0a46de5f9ca123b222cf005462
Shawn McCool <shawn@heybigname.com>
2013-05-26 21:44:14 +0200

    refactoring cookbooks to submodules

---
+++ chef/cookbooks/vagrant-main/README.md
@@ -0,0 +1,13 @@
+Description
+===========
+This is the primary location of systems customization and logic for our hosted Chef setups.
+
+Requirements
+============
+
+Attributes
+==========
+
+Usage
+=====
+
---
+++ chef/cookbooks/vagrant-main/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Big Name"
+maintainer_email "shawn@heybigname.com"
+license          "All rights reserved"
+description      "Installs/Configures vagrant_main"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.18"
