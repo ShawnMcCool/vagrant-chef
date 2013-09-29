a31a141c3e2ef57c2c49344f46a9db9c110117af
Shawn McCool <shawn@heybigname.com>
2013-09-29 10:34:51 +0200

    add umask to shell provisioner

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -2,4 +2,5 @@
 
 if [ -n /home/vagrant/.bash_profile ]; then
     cp /vagrant/vagrant-chef/scripts/files/.bash_profile /home/vagrant
+    cat /vagrant/vagrant-chef/scripts/files/etc_profile >> /etc/profile
 fi
