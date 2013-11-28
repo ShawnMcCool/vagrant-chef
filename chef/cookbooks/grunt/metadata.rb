470430f6ebe15de55b24750a47eacea3f3ba9e6e
Spencer Deinum <spencerdeinum@gmail.com>
2013-11-28 00:59:09 -0500

    Add recipe for grunt-cli
    Signed-off-by: Spencer Deinum <spencerdeinum@gmail.com>

---
+++ chef/cookbooks/grunt/metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "Spencer Deinum"
+maintainer_email "spencerdeinum@gmail.com"
+description      "Installs/Configures grunt-cli"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+
+depends 'nodejs'
